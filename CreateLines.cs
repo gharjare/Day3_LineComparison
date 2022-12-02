f5254200d7fc4556dcb3da8a550c100a0a380a95
gharjare <gharjarechetan@gmail.com>
2022-12-02 19:06:25 +0530

    new line

---
+++ CreateLines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComarisonProblem
+{
+    public class CreateLines
+    {
+      public static void CheckLength()
+      {
+            int x1 = 4;
+            int x2 = 5;
+            int y1 = 6;
+            int y2 = 7;
+
+            double lengthOfLine1 = Math.Sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+            Console.WriteLine("The points x1 = " + x1);
+            Console.WriteLine("The points x2 = " + x2);
+            Console.WriteLine("The points y1 = " + y1);
+            Console.WriteLine("The points y2 = " + y2);
+            
+      }
+        
+
+    }
+}
