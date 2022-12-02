346ecbf3c5b0bd529edd800f90955c45af4b4961
gharjare <gharjarechetan@gmail.com>
2022-12-02 19:04:46 +0530

    Wlcm msg

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LineComarisonProblem;
+
+namespace LineComparisonProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Problem");
+
+           // CreateLines.CheckLength();
+            //ComparingTwoLines.ComparingLines();
+            CompareTo.MethodCompare();
+        }
+    }
+}
